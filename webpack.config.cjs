08c749c1b30d344c84802abff9237432b4e4ac19
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-11 07:45:04 -0400

    feat: bump version and switch to better workflow

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const config = {
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   devServer: {
